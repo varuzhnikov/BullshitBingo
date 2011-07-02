dbc82f3393ce914a883a0eaae474614be5fbe877
Anton Katkov <katkov.anton@gmail.com>
2011-07-02 11:34:00 +0400

    added unit tests

---
+++ BullshitIpadTest/BullshitIpadTest.h
@@ -0,0 +1,17 @@
+//
+//  BullshitIpadTest.h
+//  BullshitIpadTest
+//
+//  Created by Vanger on 02.07.11.
+//  Copyright 2011 Flexis. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface BullshitIpadTest : SenTestCase {
+@private
+    
+}
+
+@end
